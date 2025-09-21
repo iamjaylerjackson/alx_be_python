3663b308c3cf4ad9a9d9f8505e6783e56a3d380a
Richard Sam Jackson <info.rsamjackson@gmail.com>
2025-09-21 21:36:22 +0000

    Commiting daiyl reminder and pattern drawing.

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,29 @@
+task = input("What is your task description? ")
+priority = input("Task priority (high, medium, low)? ")
+time_bound = input("Is the task is time-bound (yes or no)? ")
+
+match priority:
+    case "high":
+        if time_bound == "yes":
+            print(
+                f"Reminder: {task} is a high priority task that requires immediate attention today!")
+        else:
+            print(
+                f"Reminder: {task} is a high priority task, try and complete when you can.")
+
+    case "medium":
+        if time_bound == "yes":
+            print(
+                f"Reminder: {task} is a meduim priority task that require iimediate atention today!")
+        else:
+            print(
+                f"Reminder: {task} is a medium priority task, try and complete it when you can.")
+
+    case "low":
+        if time_bound == "yes":
+            print(
+                "Reminder: {task} is a low priority task that requires immediate attention today!")
+
+        else:
+            print(
+                f"Note: {task} is a low priority task, consider completing it when you have free time.")
