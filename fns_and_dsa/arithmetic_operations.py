79b619c6f4d11663aa2bbf8f4080ff3c437508b8
Richard Sam Jackson <info.rsamjackson@gmail.com>
2025-09-28 21:56:24 +0000

    num1 + num2 issue taken care of

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,7 +9,7 @@ def perform_operation(num1, num2, operation):
 
     match operation:
         case "add":
-            return num1 + num1
+            return num1 + num2
 
         case "subtract":
             return num1 - num2
