52a52edae486837bd87f4270e7d1a97e5c09c740
Richard Sam Jackson <info.rsamjackson@gmail.com>
2025-09-28 23:17:06 +0000

    update shopping list

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -2,7 +2,7 @@ shopping_list = []
 
 
 def display_menu():
-    print("\nShopping List Manager")
+    print("Shopping List Manager")
     print("1. Add item")
     print("2. Remove item")
     print("3. View list")
@@ -27,7 +27,7 @@ while True:
             print(f"{item} was not found in your list.")
 
     elif choice == "3":
-        print("\nYour Shopping List:")
+        print("Your Shopping List:")
         if shopping_list:
             for i, item in enumerate(shopping_list, start=1):
                 print(f"{i}. {item}")
