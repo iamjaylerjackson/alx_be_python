1e6fb70913df738a5166b3cca6172a3fd778e5c4
Richard Sam Jackson <info.rsamjackson@gmail.com>
2025-10-12 22:13:14 +0000

    Commiting Library system

---
+++ oop/library_system.py
@@ -0,0 +1,43 @@
+# library_system.py
+
+class Book:
+    def __init__(self, title: str, author: str):
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return f"'{self.title}' by {self.author}"
+
+
+class EBook(Book):
+    def __init__(self, title: str, author: str, file_size: int):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+    def __str__(self):
+        return f"E-Book: '{self.title}' by {self.author} {self.file_size}KB"
+
+
+class PrintBook(Book):
+    def __init__(self, title: str, author: str, page_count: int):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+    def __str__(self):
+        return f"Print Book: '{self.title}' by {self.author} {self.page_count}"
+
+
+class Library:
+    def __init__(self):
+        self.books = []  # composition: a library *has* books
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def list_books(self):
+        if not self.books:
+            print("No books in the library.")
+        else:
+            print("Books in the library:")
+            for book in self.books:
+                print(f" {book}")
